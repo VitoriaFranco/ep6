5aeef5433878f90a672df7f183c14735bdaa94a3
VitoriaFranco <128157948+VitoriaFranco@users.noreply.github.com>
2023-10-19 02:14:59 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ function funcao_resposta(){
             contador = contador + 1;
         }
         else{
-            document.getElementById("resp_errada_1").innerHTML = "Errada, pois a soma dos quadrados dos catetos é igual ao quadrada da hipotenusa!"
+            document.getElementById("resp_errada_1").innerHTML = "Errada. Explicação: Usando a equação de Coulomb:F = k·q¹·q² / r²  Suponha que K = 9 x 10⁹ Nm²/C² (constante eletrostática no vácuo). F = (9 x 10⁹ Nm²/C²)·(3C)·(2C)/(4m)²  F = 54 x 10⁹ Nm²/C²/16m²  F = 54 x 10⁹/16 N  F = 3.375 x 10⁹ N Portanto, a força elétrica entre essas partículas é 3.375 x 10⁹ N, que é aproximadamente 6N, portanto a resposta correta é a letra B!"  
 
         }
         if(resp2.value == "correta"){
@@ -33,7 +33,7 @@ function funcao_resposta(){
             contador = contador + 1;
         }
         else{
-            document.getElementById("resp_errada_2").innerHTML = "Errada, pois a soma dos quadrados dos catetos é igual ao quadrada da hipotenusa!"
+            document.getElementById("resp_errada_2").innerHTML = "Errada, Explicação: Usando a mesma equação, mas rearranjando-a para encontrar a carga q2:  q2 = F·r²/k·q¹ Suponha que k = 9 x 10⁹ Nm²/C² (constante eletrostática no vácuo). q2 = (12N)·(2m)²/(9 x 10⁹ Nm²/C²)·(4C)  q2 = 48N·4m²/36 x 10⁹ Nm²/C·C  q2 = 192N·m²/36 x 10⁹N·m²/C  q2 = 192/36 C  q2 = 5.333 x 10-⁸ C  Portanto, a carga q2 é de 2C, portanto a resposta correta é a letra B!"
 
         }
         if(resp3.value == "correta"){
@@ -41,7 +41,7 @@ function funcao_resposta(){
             contador = contador + 1;
         }
         else{
-            document.getElementById("resp_errada_3").innerHTML = "Errada, pois a soma dos quadrados dos catetos é igual ao quadrada da hipotenusa!"
+            document.getElementById("resp_errada_3").innerHTML = "Errada, Explicação: Quando a distância entre as partículas é reduzida para um quarto do valor original, a nova força F' pode ser calculada usando a equação de Coulomb:  F' =  k·q¹·q²/(r/4)²  F' =  k·q¹·q²/(r²/16)  F' = 16·k·q¹·q²/r²  F' = 16·16N  F' = 256N  Portanto, a nova força entre essas partículas é 64N portanto a resposta correta é a letra D!"
 
         }
         if(resp4.value == "correta"){
@@ -49,13 +49,50 @@ function funcao_resposta(){
             contador = contador + 1;
         }
         else{
-            document.getElementById("resp_errada_4").innerHTML = "Errada, pois a soma dos quadrados dos catetos é igual ao quadrada da hipotenusa!"
+            document.getElementById("resp_errada_4").innerHTML = "Errada, pois a força elétrica é medida em Newtons (N),  portanto a resposta correta é a letra A !"
 
         }
-        document.getElementById("quantos_acertos").innerHTML = "Você acertou " + contador + " questões!";
+       
+
+}
+
+if(resp5 && resp6 && resp7 && resp8){
+    if(resp1.value == "correta"){
+        document.getElementById("resp_correta_5").innerHTML = "Correta!"
+        contador = contador + 1;
+    }
+    else{
+        document.getElementById("resp_errada_5").innerHTML = "Errada, pois a força elétrica é inversamente proporcional ao quadrado da distância, então, quando a distância é dobrada, a força diminui pela metade,  portanto a resposta correta é a letra B!"
+
+    }
+    if(resp2.value == "correta"){
+        document.getElementById("resp_correta_6").innerHTML = "Correta!"
+        contador = contador + 1;
+    }
+    else{
+        document.getElementById("resp_errada_6").innerHTML = "Errada, pois !"
+
+    }
+    if(resp3.value == "correta"){
+        document.getElementById("resp_correta_7").innerHTML = "Correta!"
+        contador = contador + 1;
+    }
+    else{
+        document.getElementById("resp_errada_7").innerHTML = "Errada, pois de acordo com a Lei de Coulomb, a força é diretamente proporcional ao produto das cargas das partículas, portanto a resposta correta é a letra C!"
+
+    }
+    if(resp4.value == "correta"){
+        document.getElementById("resp_correta_8").innerHTML = "Correta!"
+        contador = contador + 1;
+    }
+    else{
+        document.getElementById("resp_errada_8").innerHTML = "Errada, pois a força elétrica é medida em Newtons (N), !"
+
+    }
+
+ document.getElementById("quantos_acertos").innerHTML = "Você acertou " + contador + " questões!";
     }
     else{
         alert("não deixe questões em branco!")
     }
-
 }
